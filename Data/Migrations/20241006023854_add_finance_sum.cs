7d8111f33ba9d26cc5cd8e3d9ab842c014365dc0
Aoryuma <aumaric314159@gmail.com>
2024-10-06 11:55:27 +0900

    git変更をコミット

---
+++ Data/Migrations/20241006023854_add_finance_sum.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OFAMA.Data.Migrations
+{
+    public partial class add_finance_sum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
